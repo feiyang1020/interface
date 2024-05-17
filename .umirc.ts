87053a6eae3c57ed73661b7eea964ab813589417
feiyang1020 <feiyangl1020@163.com>
2024-05-17 12:33:42 +0800

    init

---
+++ .umirc.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from "umi";
+
+export default defineConfig({
+  routes: [
+    { path: "/", component: "index" },
+    { path: "/docs", component: "docs" },
+  ],
+  npmClient: "pnpm",
+  chainWebpack(config,{env,webpack}) {
+    config.set("experiments", {
+      ...config.get("experiments"),
+      asyncWebAssembly: true,
+    });
+    config.plugin('NodePolyfillPlugin').use(webpack.ProvidePlugin, [{
+      process: "process/browser.js",
+    }]);
+    const REG = /\.wasm$/;
+
+    config.module.rule("asset").exclude.add(REG).end();
+
+    config.module
+      .rule("wasm")
+      .test(REG)
+      .include.add(/node_modules/)
+      .end()
+      .type("webassembly/async")
+      .end();
+  },
+});
