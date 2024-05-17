3f08c09b49f49182fdfe4c45e7a2668cb6a933c4
feiyang1020 <feiyangl1020@163.com>
2024-05-17 13:48:51 +0800

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,17 @@
 import yayJpg from '../assets/yay.jpg';
 import * as _secp256k1 from "tiny-secp256k1";
-_secp256k1.__initializeContext()
+import { fromHex, toHex } from "uint8array-tools";
+import { UnspentOutput, txHelpers } from '@unisat/wallet-sdk';
+const EMPTY_BUFFER = new Uint8Array(0);
+function toUint8Array(value) {
+  if (typeof value !== "string") return value;
+  if (value.match(/^\d{1,20}$/)) return parseInt(value);
+  const data = fromHex(value);
+  return toHex(data) === value ? data : EMPTY_BUFFER;
+}
+const a=async()=>{
+  txHelpers.sendRunes()
+}
 export default function HomePage() {
   return (
     <div>
