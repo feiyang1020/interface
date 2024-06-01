344c630c11b04110efb8a7cddf462bc770487428
feiyang1020 <feiyangl1020@163.com>
2024-06-01 20:03:08 +0800

    fix

--- src/components/SignUp/index.tsx
+++ src/components/SignUp/index.tsx
@@ -1,9 +1,9 @@
 import { ArrowRightOutlined } from "@ant-design/icons";
-import { Button, ConfigProvider, message } from "antd";
+import { Avatar, Button, ConfigProvider, message } from "antd";
 import { useModel } from "umi";
 
 export default () => {
-  const { connect } = useModel("global");
+  const { connect, connected, userInfo, disConnect } = useModel("global");
   const handleLogin = async () => {
     try {
       await connect();
@@ -11,27 +11,33 @@ export default () => {
     } catch (err) {}
   };
   return (
-    <ConfigProvider
-      theme={{
-        components: {
-          Button: {
-            colorPrimary: `linear-gradient(135deg, #B193CB, #60E4DE)`,
-            colorPrimaryHover: `linear-gradient(135deg, #B193CB, #60E4DE)`,
-            colorPrimaryActive: `linear-gradient(135deg, #B193CB, #60E4DE)`,
-            lineWidth: 0,
-          },
-        },
-      }}
-    >
-      <Button
-        type="primary"
-        shape="round"
-        icon={<ArrowRightOutlined />}
-        iconPosition="end"
-        onClick={handleLogin}
-      >
-        Sign up
-      </Button>
-    </ConfigProvider>
+    <>
+      {connected ? (
+        <Avatar onClick={disConnect}>{userInfo.id}</Avatar>
+      ) : (
+        <ConfigProvider
+          theme={{
+            components: {
+              Button: {
+                colorPrimary: `linear-gradient(135deg, #B193CB, #60E4DE)`,
+                colorPrimaryHover: `linear-gradient(135deg, #B193CB, #60E4DE)`,
+                colorPrimaryActive: `linear-gradient(135deg, #B193CB, #60E4DE)`,
+                lineWidth: 0,
+              },
+            },
+          }}
+        >
+          <Button
+            type="primary"
+            shape="round"
+            icon={<ArrowRightOutlined />}
+            iconPosition="end"
+            onClick={handleLogin}
+          >
+            Sign up
+          </Button>
+        </ConfigProvider>
+      )}
+    </>
   );
 };
--- src/models/global.ts
+++ src/models/global.ts
@@ -175,5 +175,6 @@ export default () => {
     loginModalShow,
     disConnect,
     updateToken,
+    userInfo
   };
 };
