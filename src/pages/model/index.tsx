11d66d831fcce6893cbb939daff2f9b8b86a493e
feiyang1020 <feiyangl1020@163.com>
2024-09-28 20:31:23 +0800

    styles

--- src/pages/model/index.tsx
+++ src/pages/model/index.tsx
@@ -4,6 +4,7 @@ import { getModel } from "@/services/api";
 import { MdEditor, MdCatalog, MdPreview } from 'md-editor-rt';
 import 'md-editor-rt/lib/preview.css';
 import './index.less';
+import { Card, Col, Divider, Row, Spin } from "antd";
 
 
 export default () => {
@@ -15,6 +16,7 @@ export default () => {
     const [model, setModel] = useState<API.ModelItem>();
     const _getModel = useCallback(async () => {
         if (!id) return
+        setLoading(true)
         const { data } = await getModel({ id: Number(id) })
         if (!data) return
         const describe = data.description;
@@ -23,6 +25,7 @@ export default () => {
             data.description = await fetch(describe).then(res => res.text());
             console.log(data.description);
         }
+        setLoading(false)
         setModel(data)
     }, [id])
     useEffect(() => {
@@ -30,13 +33,28 @@ export default () => {
     }, [_getModel]);
     const [id1] = useState('preview-only');
     const [scrollElement] = useState(document.documentElement);
-    return <div className='roadMapWrap'>
-        {model && <div className='roadMapContent'>
+    return <div className='modelPage'>
+        <Spin spinning={loading}>
+
+        </Spin>
+        {model && <div className="modelPageContent">
             {
                 model.name
             }
+            <Card style={{background:'rgba(255,255,255,0.2)'}} bordered={false}>
+                <Row>
+                    <Col>
+                        <div className="descTitle">
+                            Introduction
+                        </div>
+                        <Divider />
+                        <MdPreview modelValue={model.description} theme="dark" language="en-US" />
+                    </Col>
+                </Row>
+               
+            </Card>
 
-            <MdPreview modelValue={model.description} theme="dark" language="en-US" />
+           
 
         </div>}
     </div>
