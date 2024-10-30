a8e56882332b01ed42373c3f63f14a2461141d70
feiyang1020 <feiyangl1020@163.com>
2024-10-31 00:33:14 +0800

    fuix

--- src/components/ModelCard/UserModel.tsx
+++ src/components/ModelCard/UserModel.tsx
@@ -90,7 +90,7 @@ export default ({ model, onLike, onBuy, onDislike, onPreview, onHate, onHateCanc
                     <div className="cliamable" style={{ display: 'flex', alignItems: 'center', gap: 8,flexWrap:'nowrap',whiteSpace:'nowrap' }}>
                         Your Contribution:  <img src={coin} alt="" width={16} height={16} />{claimInfo.balance}
                     </div>
-                    <Button loading={loading} type='primary' disabled={claimInfo.balance === 0} onClick={(e) => { e.stopPropagation(); handleClaim() }} > Claim </Button>
+                    <Button loading={loading} type='primary'  onClick={(e) => { e.stopPropagation(); handleClaim() }} > Claim </Button>
                 </div>
 
 
